1efc76b12af1cd3f18ce6f7b7b584bb30f3ccf84
Magicard <mice.graff@gmail.com>
2025-10-20 21:34:07 +0100

    change to fellowship and slight changes

--- wow.py
+++ wow.py
@@ -4,17 +4,17 @@ import time
 import random
 
 def main():
-    print("Loaded... watching for WoW window and sending kess presses when wow window active")
-    wowTitle = "World of Warcraft"
+    print("Loaded... watching for Fellowship window and sending key presses when game window active")
+    gameTitle = "Fellowship"
     starttime=time.time()
     keys = ['left', 'right', 'up', 'down', 'space']
     while True:
         currentWindow = GetWindowText(GetForegroundWindow())
         key = random.randint(0, 4)
-        if(currentWindow == wowTitle):
-            print("Current window is WoW... pressing key: " + keys[key])
+        if(currentWindow == gameTitle):
+            print("Current window is Fellowship... pressing key: " + keys[key])
             pyautogui.press(keys[key])
-        interval = random.randint(1, 120)
+        interval = random.randint(1, 60)
         time.sleep(interval - ((time.time() - starttime) % interval))
 
 if __name__ == "__main__":
